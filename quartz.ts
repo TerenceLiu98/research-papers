5a93202bff82505c9cbc5fb4df18fe8014baacac
TerenceLiu <terenceliu1012@gmail.com>
2026-08-26 11:55:45 +0100

    Update site navigation and layout

--- quartz.ts
+++ quartz.ts
@@ -1,4 +1,13 @@
+import * as ExternalPlugin from "./.quartz/plugins"
 import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"
+
+type ExplorerNode = {
+  slugSegment?: string
+}
+
+ExternalPlugin.Explorer({
+  filterFn: (node: ExplorerNode) => node.slugSegment !== "tags" && node.slugSegment !== "papers",
+})
 
 const config = await loadQuartzConfig()
 export default config
